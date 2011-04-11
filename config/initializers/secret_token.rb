dd626c28c091cf50b18956ad1c6e29582f1b5f29
HourFocus <beulanyc@gmail.com>
2011-04-11 12:32:16 -0400

    Second commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SecondApp::Application.config.secret_token = '556b0da9ec4f70643b93fbfd29b07955d48052c2e5b9798728688c98753ad502e4d9dbcbf123c0b8ea640a7638a4ff2303f1a52094b9eb8e23e142f7f8fd9ace'
